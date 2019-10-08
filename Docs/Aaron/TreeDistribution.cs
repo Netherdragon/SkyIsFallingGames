4099bd63cc1d769932dcf968266f4182734f5051
rnshrwd <rnshrwd@yahoo.com>
2019-10-07 19:17:41 -0700

    Add files via upload

---
+++ Docs/Aaron/TreeDistribution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class TreeDistribution : MonoBehaviour {
+    public GameObject trees;
+    public Vector3 center;
+    public Vector3 size;
+
+    public void SpawnTrees() {
+        Vector3 pos = center + new Vector3(UnityEngine.Random.Range(-size.x/2, size.x/2), 1f, UnityEngine.Random.Range(-size.z/2, size.z/2));
+        Instantiate(trees, pos, Quaternion.identity);
+        
+    }
+
+    
+}
+
+
