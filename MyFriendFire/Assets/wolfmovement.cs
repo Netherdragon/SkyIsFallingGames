4e032f2ca948e39f4a08ff15201ba8a13e36f8c6
Netherdragon <34104924+Netherdragon@users.noreply.github.com>
2019-10-07 19:49:20 -0700

    reuploaded project

---
+++ MyFriendFire/Assets/wolfmovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+
+public class wolfmovement : MonoBehaviour
+{
+    Transform player; // ref to players position
+    PlayerHealth playerHealth; // ref to player health
+    EnemyHealth enemyHealth; // reference to enemies health
+    UnityEngine.AI.NavMeshAgent nav; // nav mesh agent reference
+
+    private void Awake()
+    {
+        // set up references
+        player = GameObject.FindGameObjectsWithTagWithTag("Player").transform;
+        playerHealth = player.GetComponent<PlayerHealth>();
+        enemyHealth = GetComponent<EnemyHealth>();
+    }
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if wolf and player are still alive
+        if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+        {
+        nav.SetDestination(PlayerPrefs.position); // set enemy nav mesh to the player
+        }
+        else
+        {
+        nav.enabled = false;
+        }
+    }
+}
+*/
