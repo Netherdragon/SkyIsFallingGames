df7675f6d63162e13fa0cad4d9a3028861b2bc32
njosterberg <njosterberg12@outlook.com>
2019-10-20 22:12:49 -0700

    Bunny and Wolf Spawn, Wolf AI
    The wolf AI is designed to follow the player. Conditions need to be set that if the player comes into contact with the wolf, the player will lose health or will die.

---
+++ MyFriendFire/Assets/Nathan/GenerateEnemies.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateEnemies : MonoBehaviour
+{
+    public GameObject theEnemy;
+    public int xPos;
+    public int zPos;
+    public int enemyCount;
+
+    void Start()
+    {
+        StartCoroutine(EnemyDrop());
+    }
+
+    IEnumerator EnemyDrop()
+    {
+        while(enemyCount < 10)
+        {
+            xPos = Random.Range(1, 200); // x position of RandomSpawn range
+            zPos = Random.Range(1, 200); // z position of RandomSpawn range
+            Instantiate(theEnemy, new Vector3(xPos, 0.5f, zPos), Quaternion.identity);
+            yield return new WaitForSeconds(0.1f);
+            enemyCount += 1;
+        }
+    }
+
+}
---
+++ MyFriendFire/Assets/Nathan/NpcFollow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcFollow : MonoBehaviour
+{
+    public GameObject ThePlayer;
+    public float TargetDistance;
+    public float AllowedDistance = 0;
+    public GameObject TheNPC;
+    public float FollowSpeed;
+    public RaycastHit Shot;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(ThePlayer.transform);
+        if(Physics.Raycast(transform.position,transform.TransformDirection(Vector3.forward),out Shot))
+        {
+            TargetDistance = Shot.distance;
+            if(TargetDistance >= AllowedDistance)
+            {
+                FollowSpeed = 0.1f;
+                transform.position = Vector3.MoveTowards(transform.position, ThePlayer.transform.position, FollowSpeed);
+
+            }
+            else
+            {
+                FollowSpeed = 0;
+            }
+        }
+    }
+}
