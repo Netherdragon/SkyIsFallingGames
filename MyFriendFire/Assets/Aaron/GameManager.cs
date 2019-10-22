b6b9363ddb92130588ce44ddd848b417cf2f7ce8
rnshrwd <rnshrwd@yahoo.com>
2019-10-21 22:19:41 -0700

    Add files via upload

---
+++ MyFriendFire/Assets/Aaron/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] SpawnList = new GameObject[3];
+    public List<GameObject> objectsCreated = new List<GameObject>();
+    public int SpawnCount { get; set; } // property to keep track of spawned objects
+
+    private bool _initColorChange;
+	private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            var objectToSpawn = SpawnList[Random.Range(0, SpawnList.Length)];
+            int x = Random.Range(-10, 10);
+            int z = Random.Range(-10, 10);
+            var pos = new Vector3(x, 0, z);
+            GameObject go = Instantiate(objectToSpawn, pos, Quaternion.identity);
+
+            objectsCreated.Add(go);
+            
+            SpawnCount++;
+        }
+
+        if (_initColorChange == true)
+        {
+            _initColorChange = false;
+
+            foreach (var obj in objectsCreated) {
+                obj.GetComponent<MeshRenderer>().material.color = Color.green;
+            }
+            objectsCreated.Clear();
+
+        }
+	}
+
+}
---
+++ MyFriendFire/Assets/Aaron/PerlinNoise.cs
@@ -0,0 +1,52 @@
+﻿
+using UnityEngine;
+
+public class PerlinNoise : MonoBehaviour
+{
+    public int width = 256;
+    public int height = 256;
+
+    public float scale = 20f;
+    public float offsetX = 100f;
+    public float offsetY = 100f;
+
+    private void Start()
+    {
+        offsetX = Random.Range(0, 99999f);
+        offsetY = Random.Range(0, 99999f);
+    }
+
+    private void Update()
+    {
+        // ~9:14 don't need to check renderer every frame
+        // put in Start?
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = GenerateTexture();
+    }
+
+    Texture2D GenerateTexture ()
+    {
+        Texture2D texture = new Texture2D(width, height);
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Color color = CalculateColor(x, y);
+                texture.SetPixel(x, y, color);
+            }
+        }
+        texture.Apply();
+        return texture;
+    }
+
+    Color CalculateColor (int x, int y)
+    {
+        float xCoord = (float)x / width * scale + offsetX;
+        float yCoord = (float)y / height * scale + offsetY;
+        float sample = Mathf.PerlinNoise(xCoord, yCoord);
+        return new Color(sample, sample, sample);
+    }
+
+
+}
