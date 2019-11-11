44759c324e2d89fd20d2f2b6250c4b1f1a3b6e0f
njosterberg <njosterberg12@outlook.com>
2019-11-11 12:53:40 -0800

    changed a bit of the movement script

--- MyFriendFire/Assets/Chris/PlayerMovement.cs
+++ MyFriendFire/Assets/Chris/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
 	private Rigidbody playerBody;
 	private Vector3 inputVector;
+    private float speed = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +16,7 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        inputVector = new Vector3(Input.GetAxis("Horizontal") * 10f, 0, Input.GetAxis("Vertical") * 10f);
+        inputVector = new Vector3(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed);
 		transform.LookAt(transform.position + new Vector3(inputVector.x, 0, inputVector.z));
 		playerBody.velocity = inputVector;
 
