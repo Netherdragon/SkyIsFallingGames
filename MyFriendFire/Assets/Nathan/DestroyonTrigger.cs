feb3627a3e6d0b46eb4109b894eded67be851627
Nathan Osterberg <42949712+njosterberg12@users.noreply.github.com>
2019-11-27 15:41:36 -0800

    temp push

--- MyFriendFire/Assets/Nathan/DestroyonTrigger.cs
+++ MyFriendFire/Assets/Nathan/DestroyonTrigger.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class DestroyonTrigger : MonoBehaviour
 {
-    public GameObject objToDestroy;
 
-    // function to destroy game objects that get too close to the campfire
-    void onTriggerEnter(Collider other)
-    {
-
-        if (other.gameObject.tag == "Enemy") // obj specified as enemy
-        {
-            Destroy(objToDestroy); // destroy enemy
-        }
-    }
+  //  private void OnTriggerEnter(myTrigger : collider)
+  //  {
+  //      if other.gameObject.tag("Player");
+  //      {
+  //          Destroy(gameObject, 1f);
+  //      }
+  //  }
 }
