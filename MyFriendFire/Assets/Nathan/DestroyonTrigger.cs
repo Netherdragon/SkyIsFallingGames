d0b031063f3203b4e30eb43825ae35776911bbee
Nathan Osterberg <42949712+njosterberg12@users.noreply.github.com>
2019-12-04 16:23:59 -0800

    fixed stuff

--- MyFriendFire/Assets/Nathan/DestroyonTrigger.cs
+++ MyFriendFire/Assets/Nathan/DestroyonTrigger.cs
@@ -5,22 +5,30 @@ using UnityEngine;
 public class DestroyonTrigger : MonoBehaviour
 {
 
-   public GameObject Meat;
+    public GameObject Meat;
+    private bool rabbitIsAlive = true;
 
     public virtual void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player") // player tag collides with rabbit obj
         {
-            Debug.Log("Drop meat");
-            Destroy(this.gameObject); // rabbit is destroyed
-            //GameObject meatClone = (GameObject)Instantiate(Meat, transform.position, transform.rotation);
-            dropMeat();
+            rabbitIsAlive = false; // set to false when collides with player
+        }
+    }
+
+    private void Update()
+    {
+        if(rabbitIsAlive == false)
+        {
+            dropMeat(); // meat drops from rabbit on frame update
         }
     }
 
     void dropMeat()
     {
         GameObject meatClone = (GameObject)Instantiate(Meat, transform.position, transform.rotation);
+        Destroy(this.gameObject);
     }
+
 
 }
